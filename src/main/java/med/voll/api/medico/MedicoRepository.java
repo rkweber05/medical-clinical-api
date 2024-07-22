bfceadec97fd48e24fe21287e82c8b8e7b6ed24c
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2024-07-22 17:13:09 -0300

    API finalized

--- src/main/java/med/voll/api/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -1,6 +1,9 @@
 package med.voll.api.medico;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
+    Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 }
