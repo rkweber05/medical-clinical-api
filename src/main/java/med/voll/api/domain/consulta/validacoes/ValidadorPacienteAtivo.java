e5bf96777fb3e0352d3bd668c617f855dc735859
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2024-07-29 14:33:05 -0300

    Feat: Applying SOLID principles

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorPacienteAtivo.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorPacienteAtivo.java
@@ -4,9 +4,13 @@ import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.ConsultaRepository;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-public class ValidadorPacienteAtivo {
+@Component
+public class ValidadorPacienteAtivo implements ValidadorAgendamentoDeConsultas {
 
+    @Autowired
     private PacienteRepository repository;
 
     public void validar(DadosAgendamentoConsulta dados) {
--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorPacienteSemOutraCOnsultaNoDia.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorPacienteSemOutraCOnsultaNoDia.java
@@ -3,10 +3,15 @@ package med.voll.api.domain.consulta.validacoes;
 import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.ConsultaRepository;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-public class ValidadorPacienteSemOutraCOnsultaNoDia {
+@Component
+public class ValidadorPacienteSemOutraCOnsultaNoDia implements ValidadorAgendamentoDeConsultas {
 
+    @Autowired
     private ConsultaRepository repository;
+
     public void validar (DadosAgendamentoConsulta dados) {
         var primeiroHorario = dados.data().withHour(7);
         var ultimoHorario = dados.data().withHour(18);
