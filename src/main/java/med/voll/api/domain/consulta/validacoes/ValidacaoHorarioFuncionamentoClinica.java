f0866cc4f2e75ce3e040a4f25dadd8d066a2ed74
Rodrigo Kunzler Weber <rkweber05@outlook.com>
2024-07-29 13:37:53 -0300

    Feat: validation class

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidacaoHorarioFuncionamentoClinica.java
@@ -0,0 +1,20 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.DayOfWeek;
+
+public class ValidacaoHorarioFuncionamentoClinica {
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        var dataConsulta = dados.data();
+        var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        var antesDaAberturaClinica = dataConsulta.getHour() < 7;
+        var depoisDoEncerramentoDaClinica = dataConsulta.getHour() > 18;
+
+        if (domingo || antesDaAberturaClinica || depoisDoEncerramentoDaClinica) {
+            throw new ValidacaoException("Consulta fora do horário de funcionamento da clínica");
+        }
+    }
+}
